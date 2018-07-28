21ea3a1de0010183d98a9c717d58c3f972b4b3ce
mingchou <419439813@qq.com>
2018-07-28 22:05:18 +0800

    项目初始化

---
+++ helloworld/helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by mingchou on 2018/7/27.
+//  Copyright © 2018年 mingchou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/helloworld/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  helloworld
+//
+//  Created by mingchou on 2018/7/27.
+//  Copyright © 2018年 mingchou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"start");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
